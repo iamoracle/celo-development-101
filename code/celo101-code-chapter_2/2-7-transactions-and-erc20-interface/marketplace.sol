b624cc6c66de2e6374d2067665bd38362a058746
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-03-08 22:22:31 +0100

    Optimization: Removed totalsupply
    Is the total supply function needed?

--- code/celo101-code-chapter_2/2-7-transactions-and-erc20-interface/marketplace.sol
+++ code/celo101-code-chapter_2/2-7-transactions-and-erc20-interface/marketplace.sol
@@ -6,7 +6,6 @@ interface IERC20Token {
   function transfer(address, uint256) external returns (bool);
   function approve(address, uint256) external returns (bool);
   function transferFrom(address, address, uint256) external returns (bool);
-  function totalSupply() external view returns (uint256);
   function balanceOf(address) external view returns (uint256);
   function allowance(address, address) external view returns (uint256);
 
